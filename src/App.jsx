07330f6f4e8d51bf70663960068635177dd8430c
SkyDe <sky_de1991@outlook.com>
2023-09-23 02:39:44 +0330

    signIn page UI implenented

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,44 @@
 /* eslint-disable no-unused-vars */
 
 // Styles
-import './App.css'
+import './App.css';
 
 // Imports
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // Pages
-import Dashboard from './Pages/Dashboard'
-import Log from './Pages/Log'
-import Profile from './Pages/Profile'
-import Notifications from './Pages/Notifications'
-import Settings from './Pages/Settings'
+import Dashboard from './Pages/Dashboard';
+import Log from './Pages/Log';
+import Profile from './Pages/Profile';
+import Notifications from './Pages/Notifications';
+import Settings from './Pages/Settings';
 
-import Login from './Pages/Login'
-import Register from './Pages/Register'
+import Login from './Pages/Login';
+import Register from './Pages/Register';
 
 // Components
-import Header from './Components/Header'
-import Aside from './Components/Aside'
+import Header from './Components/Header';
+import Aside from './Components/Aside';
 
 function App() {
-
   return (
     <div className='App'>
       <BrowserRouter>
-      <Header/>
-      <Aside />
-        <div className="container">
+        <Header />
+        <div className='container'>
+          <Aside />
           <Routes>
-            <Route path='/' element={<Dashboard />}/>
-            <Route path='/log' element={<Log />}/>
-            <Route path='/profile' element={<Profile />}/>
-            <Route path='/notifications' element={<Notifications />}/>
-            <Route path='/settings' element={<Settings />}/>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/log' element={<Log />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/notifications' element={<Notifications />} />
+            <Route path='/settings' element={<Settings />} />
           </Routes>
         </div>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,8 +1,66 @@
 // eslint-disable-next-line no-unused-vars
-import styles from './Login.module.sass'
+import styles from './Login.module.scss';
+import Logo from '../../../public/logoLight.svg';
+import { FaRegEyeSlash } from 'react-icons/fa';
+import { FaRegEye } from 'react-icons/fa';
 
+import { useState } from 'react';
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [showPass, setShowPass] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleShowPass = () => setShowPass(!showPass);
+
   return (
-    <div>Login</div>
-  )
+    <section className={styles.login}>
+      <div className={styles.cover}>
+        <img src={Logo} alt='Logo' className={styles.logo} />
+      </div>
+      <div className={styles.formCon}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <h2 className={styles.title}>Login</h2>
+          <div className={styles.inputControl}>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              value={formData.email}
+              onChange={handleChange}
+              name='email'
+            />
+          </div>
+          <div className={styles.inputControl}>
+            <label htmlFor='password'>Password</label>
+            <input
+              type={showPass ? 'password' : 'text'}
+              value={formData.password}
+              onChange={handleChange}
+              name='password'
+            />
+            <div onClick={handleShowPass} className={styles.showPassBtn}>
+              {showPass ? <FaRegEyeSlash /> : <FaRegEye />}
+            </div>
+            <a className={styles.forgetPassLink} href='#'>
+              Esqueci a senha
+            </a>
+          </div>
+          <button type='submit' className={styles.submitBtn}>
+            Login
+          </button>
+          <a href='#' className={styles.signupLink}>
+            {' '}
+            Nao possui conta? <span>Criar uma conta</span>
+          </a>
+        </form>
+      </div>
+    </section>
+  );
 }
