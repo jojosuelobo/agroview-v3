89e3599149c1de579a174b4236462495f471f915
jojosuelobo <josuelobo2000@gmail.com>
2023-09-30 18:37:31 -0300

    log via axios

--- src/Pages/Log/index.jsx
+++ src/Pages/Log/index.jsx
@@ -1,27 +1,39 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 import styles from './Log.module.sass'
+
+// Axios 
+import { getNotifications } from '../../axios/config.js'
+import axios from "axios";
+
+// Hooks
+import { useState, useEffect } from 'react'
 
 // Icons
 import { AiOutlineWarning } from 'react-icons/ai'
 
 export default function Log() {
+  const url = 'http://localhost:3000/log'
+  const [logs, setLogs] = useState([])
+
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setLogs((response.data))
+      })
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className={styles.section}>
       <h1>Log de registro</h1>
       <div className={styles.error_list}>
         <ul className={styles.list}>
-          <li>
-            <p><AiOutlineWarning className={styles.icon} /> “TERRENO #01”: Título do terreno alterado de “TERRENO PRIMEIRO” para “TERRENO #01” </p>
-            <p>13:22 15/05</p>
-          </li>
-          <li>
-            <p><AiOutlineWarning className={styles.icon} /> Plano alterado de Básico para Corporativo </p>
-            <p>13:22 15/05</p>
-          </li>
-          <li>
-            <p><AiOutlineWarning className={styles.icon} /> Novo terreno “Plantação Venda Nova” cadastrado </p>
-            <p>13:22 15/05</p>
-          </li>
+          {logs.map(log => (
+            <li key={log.id}>
+            <p><AiOutlineWarning className={styles.icon} />{log.titulo}</p>
+            <p>{log.data}</p>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/Pages/Notifications/index.jsx
+++ src/Pages/Notifications/index.jsx
@@ -16,41 +16,27 @@ export default function Notifications() {
   const url = 'http://localhost:3000/notificacoes'
   const [notifications, setNotifications] = useState([])
 
-  const handleClick = () => {
-    console.log(notifications)
-  }
-
   useEffect(() => {
-    const url = 'http://localhost:3000/notificacoes'
     axios.get(url)
       .then(response => {
         setNotifications((response.data))
       })
-      .catch(error => console.log(error)) 
+      .catch(error => console.log(error))
   }, [])
 
 
   return (
     <div className={styles.section}>
-      <button onClick={handleClick}>oi</button>
       <h1>Log de registro</h1>
       <div className={styles.error_list}>
         <ul className={styles.list}>
-          <li>
-            <p><AiOutlineWarning className={styles.icon} /> Temperatura do terreno “TERRENO #01” está 3 ºC acima do esperado </p>
-            <p>13:22 15/05</p>
-            <p className={styles.text}>Descrição detalhada do erro. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non mollis lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non mollis lacus. </p>
-          </li>
-          <li>
-            <p><AiOutlineWarning className={styles.icon} /> O planteio do terreno “Plantação de café Venda Nova” está quase pronto para colheita </p>
-            <p>13:22 15/05</p>
-            <p className={styles.text}>Descrição detalhada do erro. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non mollis lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non mollis lacus. </p>
-          </li>
-          <li>
-            <p><AiOutlineWarning className={styles.icon} /> Níveis UV do terreno “Plantação de café Venda Nova” estão acima da média </p>
-            <p>13:22 15/05</p>
-            <p className={styles.text}>Descrição detalhada do erro. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non mollis lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non mollis lacus. </p>
-          </li>
+          {notifications.map(notification => (
+            <li key={notification.id}>
+              <p><AiOutlineWarning className={styles.icon} /> {notification.titulo} </p>
+              <p>{notification.data}</p>
+              <p className={styles.text}>{notification.descricao}</p>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
