5789692da1987a22a77ce2cc8c49b6d660bf60cb
jojosuelobo <josue.lobo@etaure.com.br>
2023-10-06 15:43:06 -0300

    dashboard axios

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,20 +1,59 @@
 import styles from './Dashboard.module.sass'
 import image from '../../../public/image.png'
 
+// Axios
+import axios from "axios";
+
+// React
+import { useState, useEffect } from 'react'
+
 // Icons
-import {HiOutlineArrowsExpand} from 'react-icons/hi'
-import {TiDeleteOutline} from 'react-icons/ti'
-import {AiOutlineEdit} from 'react-icons/ai'
+import { HiOutlineArrowsExpand } from 'react-icons/hi'
+import { TiDeleteOutline } from 'react-icons/ti'
+import { AiOutlineEdit } from 'react-icons/ai'
 
 // Components
 import Button from '../../Components/Button'
 
 export default function Dashboard() {
+
+  const url = 'http://localhost:3000/terreno'
+  const [terrenos, setTerrenos] = useState([])
+
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setTerrenos((response.data))
+      })
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className={styles.dashboard}>
       <h1>Dashboard</h1>
       <div className={styles.list}>
-        <div className={styles.terreno}>
+        {terrenos.map((terreno) =>
+          <div className={styles.terreno}>
+            <nav>
+              <HiOutlineArrowsExpand className={styles.icon} />
+              <h1>{terreno.nome}</h1>
+              <div className={styles.crud}>
+                <TiDeleteOutline className={styles.icon_delete} />
+                <AiOutlineEdit className={styles.icon_edit} />
+              </div>
+            </nav>
+            <div className={styles.content}>
+              <img src={image} />
+              <div className={styles.info}>
+                <p>Hectares: {terreno.hectares}ha</p>
+                <p>Temperatura: {terreno.clima.temperaturaHoje} ºC</p>
+                <p>{terreno.cidade}, {terreno.estado}</p>
+                <button>Exibir</button>
+              </div>
+            </div>
+          </div>
+        )}
+        {/* <div className={styles.terreno}>
           <nav>
             <HiOutlineArrowsExpand className={styles.icon}/>
             <h1>Terreno #01</h1>
@@ -32,7 +71,7 @@ export default function Dashboard() {
               <button>Exibir</button>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
 
