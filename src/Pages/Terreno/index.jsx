738fde9eaa77cdcfa946cfe94f2f968f22061c15
jojosuelobo <josuelobo2000@gmail.com>
2023-10-10 21:01:36 -0300

    clima

--- src/Pages/Terreno/index.jsx
+++ src/Pages/Terreno/index.jsx
@@ -3,6 +3,7 @@ import styles from './Terreno.module.sass'
 
 // Icons
 import { TbArrowBack } from 'react-icons/tb'
+import { BiSolidSun } from 'react-icons/bi'
 
 // Axios
 import axios from "axios";
@@ -11,7 +12,12 @@ import axios from "axios";
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
+// Router
+import { Link, useNavigate } from 'react-router-dom'
+
 export default function Terreno() {
+    const navigate = useNavigate()
+
     const { id } = useParams()
 
     const url = `http://localhost:3000/terreno/${id}`
@@ -29,7 +35,7 @@ export default function Terreno() {
         <div className={styles.content}>
             <nav className={styles.header}>
                 <div className={styles.textHeader}>
-                    <h1> <TbArrowBack className={styles.icon} /> {terreno.nome} </h1>
+                    <h1> <TbArrowBack className={styles.icon} onClick={() => navigate('/')}/> {terreno.nome} </h1>
                     <p>Criado em: 16/05/2023</p>
                 </div>
                 <ul className={styles.options}>
@@ -40,6 +46,25 @@ export default function Terreno() {
                 </ul>
             </nav>
             <div className={styles.mainContent}>
+                <img src={terreno.imagem} className={styles.imagem}/>
+                <div className={styles.info}>
+                    <div className={styles.clima}>
+                        <div className={styles.climaHoje}>
+                            <div className={styles.climaText}>
+                                <p>Hoje</p>
+                                <p className={styles.temperatura}>{terreno.hectares} ºC</p>
+                            </div>
+                            <BiSolidSun className={styles.icon}/>
+                        </div>
+                        <p>{terreno.cidade}, {terreno.estado}</p>
+                    </div>
+                    <div className={styles.solo}>
+                        <p>SOLO</p>
+                    </div>
+                    <div className={styles.historico}>
+                        <p>HISTORICO</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
