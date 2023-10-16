41a7594c05e0a3d481cdc8bb9e18804a26f0634e
jojosuelobo <josue.lobo@etaure.com.br>
2023-10-16 14:07:53 -0300

    correção de styles

--- src/Pages/Terreno/index.jsx
+++ src/Pages/Terreno/index.jsx
@@ -37,7 +37,7 @@ export default function Terreno() {
             .catch(error => console.log(error))
     }, [])
 
-    const [aba, setAba] = useState('Gerala')
+    const [aba, setAba] = useState('Geral')
 
     return (
         <div className={styles.content}>
